13dca76bd4b2abca13750e63e859552a25987d7e
Kenny Li <kenny.li@tubemogul.com>
2015-04-01 21:19:06 -0700

    by Kenny add reg expression and container with most water solution

---
+++ 10_reg_expression_matching/Solution.java
@@ -0,0 +1,52 @@
+/**
+ * Reguarly expression matching
+ * '.' Matches any single character. 
+ * '*' Matches zero or more of the preceding element
+ * The matching should cover the entire input string (not partial).
+ * The function prototype should be:
+ * bool isMatch(const char *s, const char *p)
+ *
+ *[TIPS]:
+ * 
+ *link comes here 
+ * http://articles.leetcode.com/2011/09/regular-expression-matching.html
+ *
+ */
+public class Solution{
+
+  public static void main(String args[]){
+    boolean matched = isMatch("Hello the world", "Hello*wold"); 
+    System.out.println(matched);
+    matched = isMatch("Hello the world", "Hello*world"); 
+    System.out.println(matched);
+  }
+
+  /**
+   * is match
+   */
+  public static boolean isMatch(String matchedStr, String exp){
+
+    //System.out.println("string: " + matchedStr + " | exp:" + exp);
+    char [] matchedChars = matchedStr.toCharArray();
+    char [] expChars = exp.toCharArray();
+
+    int indexMC = 0;
+    int indexEx = 0;
+
+    if(matchedStr.length() == 1)
+      return matchedChars[indexMC] == expChars[indexEx];
+
+    if(indexEx < (expChars.length - 1 ) && expChars[indexEx] != '*'){
+       return  (matchedChars[indexMC] == expChars[indexEx] || matchedChars[indexMC] == '.') && (isMatch(matchedStr.substring(indexMC + 1), exp.substring(indexEx + 1)));
+    }
+    //the expr char is *
+    while(indexMC < matchedChars.length && !isMatch(matchedStr.substring(indexMC), exp.substring(indexEx + 1))){
+         indexMC++;
+    }
+    if(indexMC == matchedChars.length){
+      return false; 
+    }else{
+      return true;
+    }
+  }
+}
