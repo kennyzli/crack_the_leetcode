c2fbc815dadf5657ff5eaef953f12512aad3b306
Kenny Li <kenny.li@tubemogul.com>
2015-04-03 15:14:39 -0700

    by Kenny 3 sum closet

---
+++ 16_3Sum_closest/Solution.java
@@ -0,0 +1,21 @@
+/**
+ * Given an array S of n integers, find three integers in S such that the 
+ * sum is closest to a given number, target. 
+ * Return the sum of the three integers. You may assume that each input would have exactly one solution.
+ *
+ * For example, given array S = {-1 2 1 -4}, and target = 1.
+ *
+ * The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ * maybe we can simplify the 3 sum closest to change the target -- and ++ and see
+ * is there any 3 sum for the number. 
+ */
+public class Solution{
+
+   public static void main(String args[]){
+
+   }
+
+   public static int threeSumClosest(int [] num, int target){
+        
+   }
+}
