da00725d8c54419d7b82f82205baccb2383850d5
Kenny Li <kenny.li@tubemogul.com>
2015-04-08 16:49:44 -0700

    by Kenny more changes

---
+++ 28_implement_strStr/Solution.java
@@ -0,0 +1,17 @@
+/**
+ *Implement strStr()
+ * Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ *
+ *
+ */
+public class Solution {
+
+   public static void main(String args[]){
+
+   }
+
+
+   public static int strStr(String haystack, String needle) {
+             
+   }
+}
---
+++ 29_divid_2_integers/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * Divide two integers without using multiplication, 
+ * division and mod operator.
+ * If it is overflow, return MAX_INT. 
+ *
+ */
+public class Solution{
+
+  public static void main(String args[]){
+
+  }
+
+  public static int divide(int dividend, int divisor) {
+               
+  }
+}
