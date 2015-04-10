7389be24ff44a93cb32c17c2fe5a9b32ccac122d
Kenny Li <kenny.li@tubemogul.com>
2015-04-10 16:18:43 -0700

    by kenny search insert position

---
+++ 35_search_insert_position/Solution.java
@@ -0,0 +1,24 @@
+/**
+ *Given a sorted array and a target value, return the index if the target is found. 
+ * If not, return the index where it would be if it were inserted in order.
+ * You may assume no duplicates in the array.
+ * Here are few examples.
+ * [1,3,5,6], 5 → 2
+ * [1,3,5,6], 2 → 1
+ * [1,3,5,6], 7 → 4
+ * [1,3,5,6], 0 → 0
+ *
+ * Solution: 
+ *  binary search the node and expand left or right if the target is less than the left go bin search on the bottom section
+ *  otherwise search for the top section
+ */
+public class Solution{
+
+  public static void main(String args[]){
+
+  }
+
+  public static int searchInsert(int[] A, int target) {
+
+  }
+}
