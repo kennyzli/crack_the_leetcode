0035eaff0abc25ac42887387d3d4cfe530898de0
Kenny Li <kenny.li@tubemogul.com>
2015-04-10 22:02:25 -0700

    by kenny more solutions

---
+++ 37_sdoku_solver/Solution.java
@@ -0,0 +1,15 @@
+/**
+ * Write a program to solve a Sudoku puzzle by filling the empty cells.
+ * Empty cells are indicated by the character '.'.
+ * You may assume that there will be only one unique solution.
+ *
+ */
+public class Solution{
+  
+  public static void main(String args[]){
+    
+  }
+  public void solveSudoku(char[][] board) {
+
+  }
+}
