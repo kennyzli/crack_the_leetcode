2266838abf8045c988ab9866f351a42c345be417
Kenny Li <kenny.li@tubemogul.com>
2015-04-26 17:00:19 -0700

    by Kenny max rect soution"

---
+++ 84_largest_rect_in_histogram/Solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+/*
+ *Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+ * 2 solution 
+ * 1. for each i go all the way left or right to find the max area n*n running time
+ * 2. for each i maintain a incremential stack. which oly the index of each incremential area
+ *
+ * the best solution comes to here
+ * http://fisherlei.blogspot.com/2012/12/leetcode-largest-rectangle-in-histogram.html
+ * only need O(n) running time;
+ */
+public class Solution{
+   public static void main(String args[]){
+
+   }
+
+   public int largestRectangleArea(int[] height) {
+      int sum = 0;   
+      Stack<Integer> stack = new Stack<Integer>();
+      for(int i =0; i < height.length; i++){
+        if(stack.empty() || height[stack.peek()] < height[i] ){
+           stack.add(i);
+        }else{
+           int tmp = stack.pop();
+           sum = Math.max(sum, height[i] * (i - stack.peek() -1));
+        }
+      }
+
+      return sum;
+   }
+}
